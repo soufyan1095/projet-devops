9d24575752c3a4cbc6f4a7d356e9177f1b56f1f2
soufyan1095 <soufian1095@gmail.com>
2026-04-06 13:57:43 +0100

    first commit

---
+++ formations.php
@@ -0,0 +1,8 @@
+<?php
+echo "<h2>Liste des formations</h2>";
+echo "<ul>";
+echo "<li>DevOps</li>";
+echo "<li>Docker</li>";
+echo "<li>PHP</li>";
+echo "</ul>";
+?>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1>Bienvenue sur notre plateforme</h1>";
+echo "<a href='formations.php'>Voir les formations</a>";
+?>
